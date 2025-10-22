09e677a74fcb1f9813bae67436c203bb0db87e7c
ника <stepanecnikusa@gmail.com>
2025-10-22 15:10:59 +0300

    Создала базовую структуру: класс TextStatistics для хранения статистики и основную программу

--- ISIP523_Stepanets/Program.cs
+++ ISIP523_Stepanets/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ISIP523_Stepanets
 {
-    internal class Program
+    // Класс для хранения статистики по тексту
+    public class TextStatistics
     {
-        static void Main(string[] args)
+        public string Text { get; set; }
+        public int WordCount { get; set; }
+        public string ShortestWord { get; set; }
+        public string LongestWord { get; set; }
+        public int SentenceCount { get; set; }
+        public int VowelCount { get; set; }
+        public int ConsonantCount { get; set; }
+        public Dictionary<char, int> LetterFrequency { get; set; }
+        public DateTime AnalysisTime { get; set; }
+
+        public TextStatistics()
         {
+            LetterFrequency = new Dictionary<char, int>();
+        }
+
+        public void DisplayStatistics()
+        {
+            Console.WriteLine("\n=== СТАТИСТИКА ТЕКСТА ===");
+            Console.WriteLine($"Время анализа: {AnalysisTime}");
+            Console.WriteLine($"Количество слов: {WordCount}");
+            Console.WriteLine($"Самое короткое слово: '{ShortestWord}'");
+            Console.WriteLine($"Самое длинное слово: '{LongestWord}'");
+            Console.WriteLine($"Количество предложений: {SentenceCount}");
+            Console.WriteLine($"Гласных букв: {VowelCount}");
+            Console.WriteLine($"Согласных букв: {ConsonantCount}");
+
+            Console.WriteLine("\nЧастота букв:");
+            foreach (var entry in LetterFrequency)
+            {
+                Console.WriteLine($"{entry.Key}: {entry.Value}");
+            }
+            Console.WriteLine(new string('=', 40));
         }
     }
 }
