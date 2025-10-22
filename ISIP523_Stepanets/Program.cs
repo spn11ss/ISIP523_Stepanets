d6b6bb11feb6b97ce0174b2d01a5a04e97160da0
ника <stepanecnikusa@gmail.com>
2025-10-22 14:05:50 +0300

    Создала базовую структуру: перечисление категорий товаров и класс Product с основными свойствами и методом PrintInfo()

--- ISIP523_Stepanets/Program.cs
+++ ISIP523_Stepanets/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ISIP523_Stepanets
 {
-    internal class Program
+   // Перечисление для категорий товаров
+    public enum ProductCategory
     {
-        static void Main(string[] args)
+        Electronics,
+        Clothing,
+        Books,
+        Food,
+        Sports
+    }
+    // Класс товара
+    public class Product
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public bool IsInStock => Quantity > 0;
+        public ProductCategory Category { get; set; }
+
+        public Product(string code, string name, decimal price, int quantity, ProductCategory category)
         {
+            Code = code;
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+            Category = category;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Код: {Code}");
+            Console.WriteLine($"Название: {Name}");
+            Console.WriteLine($"Цена: {Price:C}");
+            Console.WriteLine($"Количество: {Quantity}");
+            Console.WriteLine($"В наличии: {(IsInStock ? "Да" : "Нет")}");
+            Console.WriteLine($"Категория: {Category}");
+            Console.WriteLine(new string('-', 30));
         }
     }
 }
