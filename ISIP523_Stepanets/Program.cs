6b7d542df83adc0a11d9c59033811d88282d9284
ника <stepanecnikusa@gmail.com>
2025-10-22 15:17:35 +0300

    Добавила множества гласных/согласных букв и основное меню

--- ISIP523_Stepanets/Program.cs
+++ ISIP523_Stepanets/Program.cs
@@ -41,4 +41,67 @@ namespace ISIP523_Stepanets
             Console.WriteLine(new string('=', 40));
         }
     }
+    class Program
+    {
+        private static List<TextStatistics> allStatistics = new List<TextStatistics>();
+
+        // Множества гласных букв (русские и английские)
+        private static readonly HashSet<char> vowels = new HashSet<char>
+        {
+            'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я',
+            'a', 'e', 'i', 'o', 'u', 'y'
+        };
+
+        // Множества согласных букв (русские и английские)
+        private static readonly HashSet<char> consonants = new HashSet<char>
+        {
+            'б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
+            'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'
+        };
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            bool exit = false;
+
+            while (!exit)
+            {
+                DisplayMenu();
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("Функция анализа текста будет реализована в следующем коммите");
+                        break;
+                    case "2":
+                        ShowAllStatistics();
+                        break;
+                    case "3":
+                        exit = true;
+                        Console.WriteLine("До свидания!");
+                        break;
+                    default:
+                        Console.WriteLine("Неверный выбор! Попробуйте снова.");
+                        break;
+                }
+
+                Console.WriteLine("\nНажмите любую клавишу для продолжения...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        static void DisplayMenu()
+        {
+            Console.WriteLine("=== АНАЛИЗАТОР ТЕКСТА ===");
+            Console.WriteLine("1. Анализировать новый текст");
+            Console.WriteLine("2. Показать статистику по всем текстам");
+            Console.WriteLine("3. Выход");
+            Console.Write("Выберите действие: ");
+        }
+
+    }
 }
