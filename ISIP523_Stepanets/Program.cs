a6563bfa2dde299deca7fba74199a8967307d942
ника <stepanecnikusa@gmail.com>
2025-11-01 13:26:26 +0300

    Добавила метод появления врагов SpawnEnemy и боссов SpawnBoss

--- ISIP523_Stepanets/Program.cs
+++ ISIP523_Stepanets/Program.cs
@@ -55,4 +55,38 @@ public class Game
         Console.WriteLine("\nИгра окончена! Вы погибли...");
     }
 
-   
+    private void SpawnBoss()
+    {
+        Console.WriteLine("Появляется босс!");
+        Boss[] bosses = {
+            new Boss("ВВГ", "Гоблин", 2.0, 1.5, 1.2, 0.1),
+            new Boss("Ковальский", "Скелет", 2.5, 1.3, 1.4, 0),
+            new Boss("Архимаг C++", "Маг", 1.8, 1.6, 1.1, 0.1),
+            new Boss("Пестов С--", "Скелет", 1.3, 1.8, 0.6, 0.15)
+        };
+
+        Boss boss = bosses[random.Next(bosses.Length)];
+        StartCombat(boss);
+    }
+
+    private void SpawnEnemy()
+    {
+        Enemy enemy;
+        int enemyType = random.Next(3);
+        switch (enemyType)
+        {
+            case 0:
+                enemy = new Goblin();
+                break;
+            case 1:
+                enemy = new Skeleton();
+                break;
+            default:
+                enemy = new Mage();
+                break;
+        }
+        Console.WriteLine($"Появляется {enemy.Name}!");
+        StartCombat(enemy);
+    }
+
+    
