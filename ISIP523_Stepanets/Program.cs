a90e83d120c819dde6bf617def4429b497124d64
ника <stepanecnikusa@gmail.com>
2025-11-01 13:23:32 +0300

    Добавила основной игровой цикл Start "пока игрок жив - игра продолжается"

--- ISIP523_Stepanets/Program.cs
+++ ISIP523_Stepanets/Program.cs
@@ -23,4 +23,36 @@ public class Game
         turnCount = 0;
     }
 
+    public void Start()
+    {
+        Console.WriteLine("Добро пожаловать в текстовый рогалик!");
+
+        while (player.IsAlive)
+        {
+            turnCount++;
+            Console.WriteLine($"\n--- Ход {turnCount} ---");
+            Console.WriteLine($"Здоровье игрока: {player.Health}");
+
+            if (turnCount % 10 == 0)
+            {
+                SpawnBoss();
+            }
+            else
+            {
+                if (random.Next(2) == 0)
+                    SpawnEnemy();
+                else
+                    OpenChest();
+            }
+
+            if (player.IsFrozen)
+            {
+                Console.WriteLine("Вы заморожены и пропускаете ход!");
+                player.IsFrozen = false;
+            }
+        }
+
+        Console.WriteLine("\nИгра окончена! Вы погибли...");
+    }
+
    
