12576a550a8569d0a53b9c8cb8f447a4091925eb
ника <stepanecnikusa@gmail.com>
2025-11-01 13:38:11 +0300

    создала класс  Player

--- ISIP523_Stepanets/Program.cs
+++ ISIP523_Stepanets/Program.cs
@@ -197,3 +197,14 @@ public class Game
     }
 }
 
+public class Player
+{
+    public int Health { get; set; } = 100;
+    public Weapon Weapon { get; set; } = new Weapon("Ржавый меч", 10);
+    public Armor Armor { get; set; } = new Armor("Простая броня", 5);
+    public bool IsDefending { get; set; }
+    public int BlockAmount { get; set; }
+    public bool IsFrozen { get; set; }
+    public bool IsAlive => Health > 0;
+}
+
