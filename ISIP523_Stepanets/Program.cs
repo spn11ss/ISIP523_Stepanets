ca95578ca4d8fa7c0477e355f18fb3b1ba0abdac
ника <stepanecnikusa@gmail.com>
2025-11-01 13:19:41 +0300

    Добавила точку входа в программу и базовую структуру

--- ISIP523_Stepanets/Program.cs
+++ ISIP523_Stepanets/Program.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ISIP523_Stepanets
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        Game game = new Game();
+        game.Start();
     }
 }
+
