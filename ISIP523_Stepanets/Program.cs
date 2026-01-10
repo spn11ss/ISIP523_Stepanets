f2d4d78e2e7bcd2e54a1ab6d39bf06fbc4d63100
ника <stepanecnikusa@gmail.com>
2026-01-10 21:34:29 +0300

    Точка входа - Main()

--- ISIP523_Stepanets/Program.cs
+++ ISIP523_Stepanets/Program.cs
@@ -1,16 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ISIP523_Stepanets
 {
-    internal class Program
+
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+
+        bool outt = true;
+        while (outt)
         {
-            int n;
+            Console.WriteLine("\n=== ГЛАВНОЕ МЕНЮ ===");
+            Console.WriteLine("1. Регистрация");
+            Console.WriteLine("2. Вход");
+            Console.WriteLine("3. Просмотр товаров (без входа)");
+            Console.WriteLine("4. Очистить базу данных");
+            Console.WriteLine("0. Выход из программы");
+
+            Console.Write("Введите выбор: ");
+            string input = Console.ReadLine();
+
+            if (!int.TryParse(input, out int choice))
+            {
+                Console.WriteLine("Неверный формат ввода.");
+                continue;
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    Registration();
+                    break;
+
+                case 2:
+                    Login();
+                    break;
+
+                case 3:
+                    WatchProducts();
+                    break;
+
+                case 4:
+                    ClearDatabase();
+                    break;
+
+                case 0:
+                    outt = false;
+                    Console.WriteLine("До свидания!");
+                    break;
+
+                default:
+                    Console.WriteLine("Неправильный пункт меню.");
+                    break;
+            }
         }
     }
 }
+          
